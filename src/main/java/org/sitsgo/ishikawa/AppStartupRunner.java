3442352887d1b4e315b4e6874de8d7fa64cb6591
Jonathan <jo.prevo@gmail.com>
2023-12-01 17:28:18 +0100

    do not start discord bot in test profile, more tests

--- src/main/java/org/sitsgo/ishikawa/AppStartupRunner.java
+++ src/main/java/org/sitsgo/ishikawa/AppStartupRunner.java
@@ -1,13 +1,18 @@
 package org.sitsgo.ishikawa;
 
 import org.sitsgo.ishikawa.discord.DiscordBot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppStartupRunner implements ApplicationRunner {
     private final DiscordBot bot;
+
+    @Autowired
+    private Environment env;
 
     AppStartupRunner(final DiscordBot bot) {
         this.bot = bot;
@@ -15,6 +20,10 @@ public class AppStartupRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
+        if (env.matchesProfiles("test")) {
+            return;
+        }
+
         bot.login();
     }
 }
